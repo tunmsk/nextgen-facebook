571276800ba656fb37d25d0d9a95bcfe40d03226
JS Morisset <jsm@surniaulula.com>
2015-10-03 18:07:31 -0400

    v8.10.3 stable

--- lib/messages.php
+++ lib/messages.php
@@ -713,9 +713,9 @@ if ( ! class_exists( 'NgfbMessages' ) ) {
 					case 'side-purchase':
 						$text = '<p>';
 						if ( $this->p->is_avail['aop'] )
-							$text .= sprintf( __( '%s can be purchased quickly and easily via Paypal &mdash; allowing you to license and enable Pro version features within seconds of your purchase.', 'nextgen-facebook' ), $atts['short_pro'] );
-						else $text .= sprintf( __( '%s can be purchased quickly and easily via Paypal &mdash; allowing you to update the plugin within seconds of your purchase.', 'nextgen-facebook' ), $atts['short_pro'] );
-						$text .= ' '.__( 'Pro version licenses do not expire &mdash; there are no yearly or recurring fees for updates and support.', 'nextgen-facebook' );
+							$text .= sprintf( __( '%s can be purchased quickly and easily via Paypal &ndash; allowing you to license and enable Pro version features within seconds of your purchase.', 'nextgen-facebook' ), $atts['short_pro'] );
+						else $text .= sprintf( __( '%s can be purchased quickly and easily via Paypal &ndash; allowing you to update the plugin within seconds of your purchase.', 'nextgen-facebook' ), $atts['short_pro'] );
+						$text .= ' '.__( 'Pro version licenses do not expire &ndash; there are no yearly or recurring fees for updates and support.', 'nextgen-facebook' );
 						$text .= '<p>';
 						break;
 					case 'side-help':
