5a43202d135be45df818cfb3be7a278a573077de
JS Morisset <jsm@surniaulula.com>
2015-10-17 12:05:56 -0400

    v8.11.1 minor update for french translations

--- lib/website/twitter.php
+++ lib/website/twitter.php
@@ -68,32 +68,29 @@ if ( ! class_exists( 'NgfbSubmenuSharingTwitter' ) && class_exists( 'NgfbSubmenu
 			$rows[] = '<tr class="hide_in_basic">'.
 			$this->p->util->get_th( _x( 'Do Not Track',
 				'option label (short)', 'nextgen-facebook' ), 'short', null,
-			'Disable tracking for Twitter\'s tailored suggestions and tailored ads.' ).
+			__( 'Disable tracking for Twitter\'s tailored suggestions and ads feature.', 'nextgen-facebook' ) ).
 			'<td>'.$this->form->get_checkbox( 'twitter_dnt' ).'</td>';
 
 			$rows[] = $this->p->util->get_th( _x( 'Add via @username',
 				'option label (short)', 'nextgen-facebook' ), 'short', null, 
-			'Append the website\'s @username to the tweet (see the '.$this->p->util->get_admin_url( 'general#sucom-tabset_pub-tab_twitter', 'Twitter options tab' ).' on the General settings page). The website\'s @username will be displayed and recommended after the Post / Page is shared.' ).
+			sprintf( __( 'Append the website\'s business @username to the tweet (see the <a href="%1$s">Twitter</a> options tab on the %2$s settings page). The website\'s @username will be displayed and recommended after the webpage is shared.', 'nextgen-facebook' ), $this->p->util->get_admin_url( 'general#sucom-tabset_pub-tab_twitter' ), _x( 'General', 'lib file description', 'nextgen-facebook' ) ) ).
 			( $this->p->check->aop( 'ngfb' ) ? '<td>'.$this->form->get_checkbox( 'twitter_via' ).'</td>' :
 				'<td class="blank">'.$this->form->get_no_checkbox( 'twitter_via' ).'</td>' );
 
 			$rows[] = $this->p->util->get_th( _x( 'Recommend Author',
 				'option label (short)', 'nextgen-facebook' ), 'short', null, 
-			'Recommend following the Author\'s Twitter @username (from their profile) after sharing. If the \'<em>Add via @username</em>\' option (above) is also checked, the Website\'s @username is suggested first.' ).
+			sprintf( __( 'Recommend following the author\'s Twitter @username (from their profile) after sharing a webpage. If the <em>%1$s</em> option is also checked, the website\'s @username is suggested first.', 'nextgen-facebook' ), _x( 'Add via @username', 'option label (short)', 'wpsso-rrssb' ) ) ).
 			( $this->p->check->aop( 'ngfb' ) ? 
 				'<td>'.$this->form->get_checkbox( 'twitter_rel_author' ).'</td>' :
 				'<td class="blank">'.$this->form->get_no_checkbox( 'twitter_rel_author' ).'</td>' );
 
 			$rows[] = $this->p->util->get_th( _x( 'Shorten URLs with',
 				'option label (short)', 'nextgen-facebook' ), 'short', null, 
-			'If you select a URL shortening service here, <strong>you must also enter its Service API Keys</strong> on the '.
-			$this->p->util->get_admin_url( 'advanced#sucom-tabset_plugin-tab_apikeys', 'Advanced settings page' ).'.' ).
+			sprintf( __( 'If you select a URL shortening service here, you must also enter its <a href="%1$s">%2$s</a> on the %3$s settings page.', 'nextgen-facebook' ), $this->p->util->get_admin_url( 'advanced#sucom-tabset_plugin-tab_apikeys' ), _x( 'Service API Keys', 'metabox tab', 'nextgen-facebook' ), _x( 'Advanced', 'lib file description', 'nextgen-facebook' ) ) ).
 			( $this->p->check->aop( 'ngfb' ) ? 
-				'<td>'.$this->form->get_select( 'plugin_shortener', $this->p->cf['form']['shorteners'], 'short' ).'&nbsp;' :
-				'<td class="blank">'.$this->form->get_hidden( 'plugin_shortener' ).
-					$this->p->cf['form']['shorteners'][$this->p->options['plugin_shortener']].' &mdash; ' ).
-			' <strong>using '.$this->p->util->get_admin_url( 'advanced#sucom-tabset_plugin-tab_apikeys', 
-				'Service API Keys' ).'</strong></td>';
+				'<td>'.$this->form->get_select( 'plugin_shortener', $this->p->cf['form']['shorteners'], 'short' ).'&nbsp; ' :
+				'<td class="blank">'.$this->p->cf['form']['shorteners'][$this->p->options['plugin_shortener']].' &mdash; ' ).
+			sprintf( __( 'using these <a href="%1$s">%2$s</a>', 'nextgen-facebook' ), $this->p->util->get_admin_url( 'advanced#sucom-tabset_plugin-tab_apikeys' ), _x( 'Service API Keys', 'metabox tab', 'nextgen-facebook' ) ).'</td>';
 
 			return $rows;
 		}
