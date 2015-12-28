7d4e04a06ffb13c7a0a42f757d01778ca4fc93ab
JS Morisset <jsm@surniaulula.com>
2015-12-28 13:08:28 -0700

    v8.20.1.1 changed default OG from 1200x1200 to 600x315

--- lib/config.php
+++ lib/config.php
@@ -249,7 +249,7 @@ if ( ! class_exists( 'NgfbConfig' ) ) {
 					'og_site_description' => '',
 					'og_art_section' => 'none',
 					'og_img_width' => 600,
-					'og_img_height' => 600,
+					'og_img_height' => 315,
 					'og_img_crop' => 1,
 					'og_img_crop_x' => 'center',
 					'og_img_crop_y' => 'center',
