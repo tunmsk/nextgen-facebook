a20280753cd443b60f85616a7590f04f997954ee
JS Morisset <jsm@surniaulula.com>
2015-12-29 14:50:01 -0700

    v8.20.1.2 renamed setup guide in network menu

--- lib/config.php
+++ lib/config.php
@@ -74,7 +74,7 @@ if ( ! class_exists( 'NgfbConfig' ) ) {
 						'sitesubmenu' => array(	// lib file descriptions will be translated
 							'siteadvanced' => 'Advanced Settings',
 							'sitereadme' => 'Plugin Read Me',
-							'sitesetup' => 'Quick Setup Guide',
+							'sitesetup' => 'Setup Guide',
 							// the first 2 words will be highlighted in menu
 							'sitelicenses' => '<span>Extension Plugins</span> and Pro Licenses',
 						),
