1a9b13a05e4403bc0168aa17d1e91a05903bea90
JS Morisset <jsm@surniaulula.com>
2016-02-06 15:12:46 -0400

    v8.23.0 updated tested up to wp version

--- nextgen-facebook.php
+++ nextgen-facebook.php
@@ -11,7 +11,7 @@
  * License URI: http://www.gnu.org/licenses/gpl.txt
  * Description: Display your content in the best possible way on Facebook, Google+, Twitter, Pinterest, etc. - no matter how your webpage is shared!
  * Requires At Least: 3.1
- * Tested Up To: 4.4
+ * Tested Up To: 4.4.2
  * Version: 8.23.0
  * 
  * Copyright 2012-2016 Jean-Sebastien Morisset (http://surniaulula.com/)
