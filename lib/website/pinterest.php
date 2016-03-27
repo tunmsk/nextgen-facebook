90a2f30342f4b1eb1968cc5c968d6bf69879cdc5
JS Morisset <jsm@surniaulula.com>
2016-03-27 07:48:39 -0400

    v8.28.4.1 added a but more debug info

--- lib/website/pinterest.php
+++ lib/website/pinterest.php
@@ -185,13 +185,17 @@ if ( ! class_exists( 'NgfbWebsitePinterest' ) ) {
 			if ( empty( $atts['size'] ) ) 
 				$atts['size'] = $this->p->cf['lca'].'-pinterest-button';
 
-			if ( ! empty( $atts['pid'] ) )
+			if ( ! empty( $atts['pid'] ) ) {
 				list(
 					$atts['photo'],
 					$atts['width'],
 					$atts['height'],
-					$atts['cropped']
-				) = $this->p->media->get_attachment_image_src( $atts['pid'], $atts['size'], false );
+					$atts['cropped'],
+					$atts['pid']
+				) = $this->p->media->get_attachment_image_src( $atts['pid'], $atts['size'], false );	// $check_dupes = false
+				if ( $this->p->debug->enabled )
+					$this->p->debug->log( 'returned image '.$atts['photo'].' ('.$atts['width'].'x'.$atts['height'].')' );
+			}
 
 			if ( empty( $atts['photo'] ) && empty( $atts['embed'] ) ) {
 				$media_info = $this->p->og->get_the_media_info( $atts['size'], $mod, 'rp', array( 'img_url', 'vid_url' ) );
