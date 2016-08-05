6c6589454a4813687abfb901d0bc98846562a80d
JS Morisset <jsm@surniaulula.com>
2016-08-05 09:21:52 -0300

    v8.33.9-1

--- lib/config.php
+++ lib/config.php
@@ -20,7 +20,7 @@ if ( ! class_exists( 'NgfbConfig' ) ) {
 			'feed_cache_exp' => 86400,	// 24 hours
 			'plugin' => array(
 				'ngfb' => array(
-					'version' => '8.33.9-b1',	// plugin version
+					'version' => '8.33.9-1',	// plugin version
 					'opt_version' => '443',		// increment when changing default options
 					'short' => 'NGFB',		// short plugin name
 					'name' => 'NextGEN Facebook (NGFB)',
