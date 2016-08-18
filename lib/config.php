267369eb2b0d7323ccd6ad86aaeb119bc322fcc3
JS Morisset <jsm@surniaulula.com>
2016-08-18 18:42:49 -0300

    v8.34.0-1

--- lib/config.php
+++ lib/config.php
@@ -515,7 +515,7 @@ if ( ! class_exists( 'NgfbConfig' ) ) {
 					'plugin_file_cache_exp' => 0,			// File Cache Expiry
 					// Service API Keys Tab
 					'plugin_shortener' => 'none',
-					'plugin_shortlink' => 1,
+					'plugin_shortlink' => 1,			// Get Shortlink Gives Shortened URL
 					'plugin_min_shorten' => 23,
 					'plugin_bitly_login' => '',
 					'plugin_bitly_token' => '',
