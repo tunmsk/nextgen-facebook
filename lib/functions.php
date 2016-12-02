8a7699bc14fbc33da6bf7c5ea24279fae26f77eb
JS Morisset <jsm@surniaulula.com>
2016-12-02 15:11:58 -0800

    v8.37.6-dev4

--- lib/functions.php
+++ lib/functions.php
@@ -41,36 +41,34 @@ if ( ! function_exists( 'ngfb_get_sharing_buttons' ) ) {
 				( $ngfb->debug->enabled ? $ngfb->debug->get_html() : '' );
 		}
 
-		$atts['use_post'] = SucomUtil::sanitize_use_post( $atts ); 
-
 		$lca = $ngfb->cf['lca'];
 		$type = __FUNCTION__;
+		$atts['use_post'] = SucomUtil::sanitize_use_post( $atts ); 
 		$mod = $ngfb->util->get_page_mod( $atts['use_post'] );
 		$sharing_url = $ngfb->util->get_sharing_url( $mod );
-		$buttons_index = $ngfb->sharing->get_buttons_cache_index( $type, $atts, $ids );
 		$buttons_array = array();
+		$buttons_index = $ngfb->sharing->get_buttons_cache_index( $type, $atts, $ids );
+		$cache_salt = __FUNCTION__.'('.SucomUtil::get_mod_salt( $mod, $sharing_url ).')';
+		$cache_id = $lca.'_'.md5( $cache_salt );
 		$cache_exp = (int) apply_filters( $lca.'_cache_expire_sharing_buttons', 
 			( $cache_exp === false ? $ngfb->options['plugin_sharing_buttons_cache_exp'] : $cache_exp ) );
 
 		if ( $ngfb->debug->enabled ) {
 			$ngfb->debug->log( 'sharing url = '.$sharing_url );
 			$ngfb->debug->log( 'buttons index = '.$buttons_index );
-			$ngfb->debug->log( 'cache expire = '.$cache_exp );
+			$ngfb->debug->log( 'transient expire = '.$cache_exp );
+			$ngfb->debug->log( 'transient salt = '.$cache_salt );
 		}
-
-		$cache_salt = __FUNCTION__.'('.SucomUtil::get_mod_salt( $mod, null, $sharing_url ).')';
-		$cache_id = $lca.'_'.md5( $cache_salt );
-		if ( $ngfb->debug->enabled )
-			$ngfb->debug->log( 'transient cache salt '.$cache_salt );
 
 		if ( $cache_exp > 0 ) {
 			$buttons_array = get_transient( $cache_id );
 			if ( isset( $buttons_array[$buttons_index] ) ) {
 				if ( $ngfb->debug->enabled )
-					$ngfb->debug->log( $type.' buttons array retrieved from transient '.$cache_id );
-			}
+					$ngfb->debug->log( $type.' buttons index found in array from transient '.$cache_id );
+			} elseif ( $this->p->debug->enabled )
+				$ngfb->debug->log( $type.' buttons index not in array from transient '.$cache_id );
 		} elseif ( $this->p->debug->enabled )
-			$ngfb->debug->log( $type.' buttons array transient cache is disabled' );
+			$ngfb->debug->log( $type.' buttons array transient is disabled' );
 
 		if ( ! isset( $buttons_array[$buttons_index] ) ) {
 
