ed106884a7ee17106f4d13294a5718d5b7fbc92e
JS Morisset <jsm@surniaulula.com>
2016-12-20 18:19:06 -0800

    v8.37.9-dev2

--- nextgen-facebook.php
+++ nextgen-facebook.php
@@ -13,7 +13,7 @@
  * Description: Complete meta tags for the best looking shares on Facebook, Google, Pinterest, Twitter, etc - no matter how your webpage is shared!
  * Requires At Least: 3.7
  * Tested Up To: 4.7
- * Version: 8.37.9-dev1
+ * Version: 8.37.9-dev2
  *
  * Version Numbering Scheme: {major}.{minor}.{bugfix}-{stage}{level}
  *
