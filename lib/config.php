ba56e98f09631fe6fad0ce4a5bddb417313291e2
JS Morisset <jsm@surniaulula.com>
2016-12-27 15:10:24 -0800

    v8.38.1-dev3

--- lib/config.php
+++ lib/config.php
@@ -20,8 +20,8 @@ if ( ! class_exists( 'NgfbConfig' ) ) {
 			'setup_cache_exp' => 86400,	// 1 day
 			'plugin' => array(
 				'ngfb' => array(
-					'version' => '8.38.1-dev2',	// plugin version
-					'opt_version' => '478',		// increment when changing default options
+					'version' => '8.38.1-dev3',	// plugin version
+					'opt_version' => '479',		// increment when changing default options
 					'short' => 'NGFB',		// short plugin name
 					'name' => 'NextGEN Facebook (NGFB)',
 					'desc' => 'Complete meta tags for the best looking shares on Facebook, Google, Pinterest, Twitter, etc - no matter how your webpage is shared!',
