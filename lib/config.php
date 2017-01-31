b29912b57335e30578bc6cca845038c6bfb116e4
JS Morisset <jsm@surniaulula.com>
2017-01-31 15:41:33 -0800

    v8.39.6-dev1

--- lib/config.php
+++ lib/config.php
@@ -20,8 +20,8 @@ if ( ! class_exists( 'NgfbConfig' ) ) {
 			'setup_cache_exp' => 86400,	// 1 day
 			'plugin' => array(
 				'ngfb' => array(
-					'version' => '8.39.5-1',	// plugin version
-					'opt_version' => '485',		// increment when changing default options
+					'version' => '8.39.6-dev1',	// plugin version
+					'opt_version' => '486',		// increment when changing default options
 					'short' => 'NGFB',		// short plugin name
 					'name' => 'NextGEN Facebook (NGFB)',
 					'desc' => 'Complete meta tags for the best looking shares on Facebook, Google, Pinterest, Twitter, etc - no matter how your webpage is shared!',
@@ -214,7 +214,14 @@ if ( ! class_exists( 'NgfbConfig' ) ) {
 					'schema_alt_name' => '',
 					'schema_logo_url' => '',
 					'schema_banner_url' => '',
+					'schema_img_max' => 1,
+					'schema_img_width' => 800,			// must be at least 696px for Articles
+					'schema_img_height' => 1600,
+					'schema_img_crop' => 0,
+					'schema_img_crop_x' => 'center',
+					'schema_img_crop_y' => 'center',
 					'schema_desc_len' => 250,			// meta itemprop="description" maximum text length (hard limit)
+					'schema_author_name' => 'display_name',
 					// standard types
 					'schema_type_for_archive_page' => 'webpage.collection',
 					'schema_type_for_attachment' => 'webpage',
@@ -241,13 +248,7 @@ if ( ! class_exists( 'NgfbConfig' ) ) {
 					'schema_type_for_tribe_events' => 'event',
 					'schema_type_for_webpage' => 'webpage',
 					'schema_type_for_website' => 'website',
-					'schema_author_name' => 'display_name',
-					'schema_img_max' => 1,
-					'schema_img_width' => 800,		// must be at least 696px for Articles
-					'schema_img_height' => 1600,
-					'schema_img_crop' => 0,
-					'schema_img_crop_x' => 'center',
-					'schema_img_crop_y' => 'center',
+					'schema_review_item_type' => 'none',
 					'seo_desc_len' => 156,			// meta name="description" maximum text length (hard limit)
 					'seo_author_field' => '',		// default value set by NgfbOptions::get_defaults()
 					'seo_publisher_url' => '',		// (localized)
@@ -1235,7 +1236,16 @@ if ( ! class_exists( 'NgfbConfig' ) ) {
 							'residence' => 'https://schema.org/Residence',
 							'tourist.attraction' => 'https://schema.org/TouristAttraction',
 						),
-						'product' => 'https://schema.org/Product',
+						'product' => array(
+							'product' => 'https://schema.org/Product',
+							'vehicle' => array(
+								'bus.or.coach' => 'https://auto.schema.org/BusOrCoach',
+								'car' => 'https://auto.schema.org/Car',
+								'motorcycle' => 'https://auto.schema.org/Motorcycle',
+								'motorized.bicycle' => 'https://auto.schema.org/MotorizedBicycle',
+								'vehicle' => 'https://auto.schema.org/Vehicle',
+							),
+						),
 						'thing' => 'https://schema.org/Thing',
 					),
 				),
