118f811354ff740752c4871168ca5b58e7dc6c64
JS Morisset <jsm@surniaulula.com>
2017-03-15 17:19:34 -0700

    v8.40.5-rc1

--- lib/config.php
+++ lib/config.php
@@ -19,7 +19,7 @@ if ( ! class_exists( 'NgfbConfig' ) ) {
 			'setup_cache_exp' => 86400,	// 1 day
 			'plugin' => array(
 				'ngfb' => array(
-					'version' => '8.40.5-dev1',	// plugin version
+					'version' => '8.40.5-rc1',	// plugin version
 					'opt_version' => '511',		// increment when changing default options
 					'short' => 'NGFB',		// short plugin name
 					'name' => 'NextGEN Facebook (NGFB)',
