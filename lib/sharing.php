5e995fd1d4e729d93e1523b00cc35e88f732a044
JS Morisset <jsm@surniaulula.com>
2017-03-27 16:12:42 +0200

    v8.40.7-dev2

--- lib/sharing.php
+++ lib/sharing.php
@@ -181,12 +181,14 @@ jQuery("#ngfb-sidebar-header").click( function(){
 					else {
 						$css_data = fread( $fh, filesize( $buttons_css_file ) );
 						fclose( $fh );
-						if ( $this->p->debug->enabled )
+						if ( $this->p->debug->enabled ) {
 							$this->p->debug->log( 'read css from file '.$buttons_css_file );
+						}
 						foreach ( array( 
 							'plugin_url_path' => $url_path,
-						) as $macro => $value )
+						) as $macro => $value ) {
 							$css_data = preg_replace( '/%%'.$macro.'%%/', $value, $css_data );
+						}
 						$def_opts['buttons_css_'.$id] = $css_data;
 					}
 				}
@@ -363,14 +365,12 @@ jQuery("#ngfb-sidebar-header").click( function(){
 						if ( is_admin() )
 							$this->p->notice->err( sprintf( __( 'The %s file is not readable.',
 								'nextgen-facebook' ), self::$sharing_css_file ) );
-					} else {
+					} elseif ( ( $fsize = @filesize( self::$sharing_css_file ) ) > 0 &&
+						$fh = @fopen( self::$sharing_css_file, 'rb' ) ) {
 						echo '<style type="text/css">';
-						if ( ( $fsize = @filesize( self::$sharing_css_file ) ) > 0 &&
-							$fh = @fopen( self::$sharing_css_file, 'rb' ) ) {
-							echo fread( $fh, $fsize );
-							fclose( $fh );
-						}
+						echo fread( $fh, $fsize );
 						echo '</style>',"\n";
+						fclose( $fh );
 					}
 				}
 			} elseif ( $this->p->debug->enabled )
